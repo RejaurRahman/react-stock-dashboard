6848a842279bab275d259a519f6c8ee45dbe1498
Rejaur Rahman <rejaur-rahman@hotmail.co.uk>
2024-04-17 01:03:33 +0100

    FIX: missing file

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,44 @@
+import React, { useContext } from "react";
+
+import ThemeContext from "../../context/ThemeContext";
+
+import Search from "../Search/Search";
+import Overview from "../Overview/Overview";
+import Details from "../Details/Details";
+import Chart from "../Chart/Chart";
+import ThemeIcon from "../ThemeIcon/ThemeIcon";
+
+export default function Dashboard() {
+  const { darkMode } = useContext(ThemeContext)
+
+  return (
+    <div
+      className={`h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-2 p-8 font-raleway ${
+        darkMode ? "bg-gray-900 text-gray-300" : "bg-neutral-100"
+      }`}
+    >
+      <div className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1 flex justify-start items-center">
+        <div className="xl:px-32">
+          <h1 className="text-5xl">Apple Inc.</h1>
+          <Search />
+        </div>
+
+        <ThemeIcon />
+      </div>
+      <div className="md:col-span-2 row-span-4 p-2">
+        <Chart />
+      </div>
+      <div className="p-2">
+        <Overview
+          symbol={"AAPL"}
+          price={130.63}
+          change={3.63}
+          changePercent={0.67}
+        />
+      </div>
+      <div className="row-span-2 xl:row-span-3 p-2">
+        <Details />
+      </div>
+    </div>
+  )
+}
